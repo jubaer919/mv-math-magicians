9e0dfb225d107b10f75648e145d17b335d7dd4ab
jubaer919 <60255197+jubaer919@users.noreply.github.com>
2023-09-19 18:59:43 +0600

    complete the math magician project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import { Link, Route, Routes } from 'react-router-dom';
 import Calculator from './components/Calculator';
 import QuoteDisplay from './components/QuoteDisplay';
+import Home from './components/Home';
+import classes from './App.module.css';
 
 function App() {
   return (
     <>
-      <QuoteDisplay />
-      <Calculator />
+      <nav className={classes['nav-bar']}>
+        <h1>Math Magician</h1>
+        <ul className={classes['nav-list']}>
+          <li className={classes['nav-link']}><Link to="/">Home</Link></li>
+          <li className={classes['nav-link']}><Link to="/calculator">Calculator</Link></li>
+          <li className={classes['nav-link']}><Link to="/quot">Quote</Link></li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/quot" element={<QuoteDisplay />} />
+      </Routes>
     </>
   );
 }
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -10,29 +10,32 @@ function Calculator() {
     setCalculatorData(newData);
   }
   return (
-    <div className={clasess['calculator-container']}>
-      <div className={clasess.display}>
-        <div className="current-value">{calculatorData.next || calculatorData.total || '0'}</div>
+    <div className={clasess.container}>
+      <h1>let us do some math</h1>
+      <div className={clasess['calculator-container']}>
+        <div className={clasess.display}>
+          <div className="current-value">{calculatorData.next || calculatorData.total || '0'}</div>
+        </div>
+        <button type="button" onClick={() => handleButtonClick('AC')}>AC</button>
+        <button type="button" onClick={() => handleButtonClick('+/-')}>+/-</button>
+        <button type="button" onClick={() => handleButtonClick('%')}>%</button>
+        <button type="button" className="orange" onClick={() => handleButtonClick('÷')}>÷</button>
+        <button type="button" onClick={() => handleButtonClick('7')}>7</button>
+        <button type="button" onClick={() => handleButtonClick('8')}>8</button>
+        <button type="button" onClick={() => handleButtonClick('9')}>9</button>
+        <button type="button" className="orange" onClick={() => handleButtonClick('x')}>*</button>
+        <button type="button" onClick={() => handleButtonClick('4')}>4</button>
+        <button type="button" onClick={() => handleButtonClick('5')}>5</button>
+        <button type="button" onClick={() => handleButtonClick('6')}>6</button>
+        <button type="button" className="orange" onClick={() => handleButtonClick('-')}>-</button>
+        <button type="button" onClick={() => handleButtonClick('1')}>1</button>
+        <button type="button" onClick={() => handleButtonClick('2')}>2</button>
+        <button type="button" onClick={() => handleButtonClick('3')}>3</button>
+        <button type="button" className="orange" onClick={() => handleButtonClick('+')}>+</button>
+        <button type="button" className={clasess['span-two']} onClick={() => handleButtonClick('0')}>0</button>
+        <button type="button" onClick={() => handleButtonClick('.')}>.</button>
+        <button type="button" className="orange" onClick={() => handleButtonClick('=')}>=</button>
       </div>
-      <button type="button" onClick={() => handleButtonClick('AC')}>AC</button>
-      <button type="button" onClick={() => handleButtonClick('+/-')}>+/-</button>
-      <button type="button" onClick={() => handleButtonClick('%')}>%</button>
-      <button type="button" className="orange" onClick={() => handleButtonClick('÷')}>÷</button>
-      <button type="button" onClick={() => handleButtonClick('7')}>7</button>
-      <button type="button" onClick={() => handleButtonClick('8')}>8</button>
-      <button type="button" onClick={() => handleButtonClick('9')}>9</button>
-      <button type="button" className="orange" onClick={() => handleButtonClick('x')}>*</button>
-      <button type="button" onClick={() => handleButtonClick('4')}>4</button>
-      <button type="button" onClick={() => handleButtonClick('5')}>5</button>
-      <button type="button" onClick={() => handleButtonClick('6')}>6</button>
-      <button type="button" className="orange" onClick={() => handleButtonClick('-')}>-</button>
-      <button type="button" onClick={() => handleButtonClick('1')}>1</button>
-      <button type="button" onClick={() => handleButtonClick('2')}>2</button>
-      <button type="button" onClick={() => handleButtonClick('3')}>3</button>
-      <button type="button" className="orange" onClick={() => handleButtonClick('+')}>+</button>
-      <button type="button" className={clasess['span-two']} onClick={() => handleButtonClick('0')}>0</button>
-      <button type="button" onClick={() => handleButtonClick('.')}>.</button>
-      <button type="button" className="orange" onClick={() => handleButtonClick('=')}>=</button>
     </div>
   );
 }
