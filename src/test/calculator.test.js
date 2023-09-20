621020c4f3a94c6dc1217859b64a42776313dfac
Lucy W. Mwangi <100368317+lucy-sees@users.noreply.github.com>
2023-09-20 15:21:54 +0300

    Add test for button click

--- src/test/calculator.test.js
+++ src/test/calculator.test.js
@@ -8,10 +8,10 @@ test('should display the initial value as 0', () => {
   expect(display[0]).toBeInTheDocument();
 });
 
-// test('should update the display when a number button is clicked', () => {
-//   render(<Calculator />);
-//   const numberButton = screen.getByText('7');
-//   fireEvent.click(numberButton);
-//   const display = screen.getAllByText('7');
-//   expect(display[0]).toBeInTheDocument();
-// });
+test('should update the display when a number button is clicked', () => {
+  render(<Calculator />);
+  const numberButton = screen.getByText('7');
+  fireEvent.click(numberButton);
+  const display = screen.getAllByText('7');
+  expect(display[0]).toBeInTheDocument();
+});
